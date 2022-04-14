ecfe1a0e67e4b81bfcdf73dad418c5e4d68ffa6a
Anton Mishanin <antonmishanin@mail.ru>
2022-04-14 11:13:59 +0700

    custom view: view group as ViewHolder

--- app/src/main/java/com/my/mypaging3/custom_view/MyViewHolder.kt
+++ app/src/main/java/com/my/mypaging3/custom_view/MyViewHolder.kt
@@ -3,25 +3,39 @@ package com.my.mypaging3.custom_view
 import android.content.Context
 import android.graphics.Canvas
 import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
 import android.widget.LinearLayout
+import android.widget.TextView
+import com.my.mypaging3.R
 
-class SimpleViewGroup : LinearLayout {
+class MyViewHolder : FrameLayout {
 
-    constructor(context: Context) : super(context)
+    private lateinit var view: View
+
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
 
     constructor(
         context: Context,
         attrs: AttributeSet?
-    ) : super(context, attrs)
+    ) : super(context, attrs) {
+        init(context)
+    }
 
     constructor(
         context: Context,
         attrs: AttributeSet?,
         defStyleAttr: Int
-    ) : super(context, attrs, defStyleAttr)
+    ) : super(context, attrs, defStyleAttr) {
+        init(context)
+    }
 
-    init {
-        requestLayout()
+    private fun init(context: Context) {
+        view = View.inflate(context, R.layout.item_user, this)
+
+        view.findViewById<TextView>(R.id.title).text = "SDSDSDSDS"
     }
 
     override fun dispatchDraw(canvas: Canvas?) {
