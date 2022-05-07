0a237c5dffa1fb5987a98d87d3d126ecff691414
Anton Mishanin <antonmishanin@mail.ru>
2022-05-07 19:42:45 +0700

    tasks: summator

---
+++ app/src/main/java/com/my/mypaging3/tasks/Summator.kt
@@ -0,0 +1,18 @@
+package com.my.mypaging3.tasks
+
+//To run use intellij idea
+/*fun main() {
+    repeat(readln().toInt()) {
+        val params = readln().split(" ").map { it.toInt() }
+        println(params[0] + params[1])
+    }
+}*/
+
+/*
+5
+256 42
+1000 1000
+-1000 1000
+-1000 1000
+20 22
+*/
